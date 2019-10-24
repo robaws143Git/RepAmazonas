64a598e1a7c3c469074a6f088ec8b725ed1169ba
usr.admin <robaws143@gmail.com>
2019-10-23 23:14:45 -0500

    PRIMER COMMIT PARA SALUDO WEB1 HACIA REPOSITORIO NUEVO

---
+++ src/pe/CPrincipalWeb.java
@@ -0,0 +1,10 @@
+package pe;
+
+public class CPrincipalWeb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("***** Hola Sistemas Base 1 **********");
+	}
+
+}
